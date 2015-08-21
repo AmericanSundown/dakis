f2af820a51652fcaa9a4896663a5b0c5c1fda327
niekas <albertas.gimbutas@mii.vu.lt>
2015-08-21 15:21:36 +0300

    Create Experiment and Task models

--- dakis/core/migrations/0001_initial.py
+++ dakis/core/migrations/0001_initial.py
@@ -2,9 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
-import django_extensions.db.fields
 import django.utils.timezone
-import autoslug.fields
+import django_extensions.db.fields
 
 
 class Migration(migrations.Migration):
@@ -16,12 +15,37 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Experiment',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('created', django_extensions.db.fields.CreationDateTimeField(editable=False, blank=True, default=django.utils.timezone.now)),
-                ('modified', django_extensions.db.fields.ModificationDateTimeField(editable=False, blank=True, default=django.utils.timezone.now)),
-                ('slug', autoslug.fields.AutoSlugField(editable=False)),
-                ('title', models.CharField(max_length=255, verbose_name='Title')),
-                ('description', models.TextField(verbose_name='Description')),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('created', django_extensions.db.fields.CreationDateTimeField(default=django.utils.timezone.now, blank=True, editable=False)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(default=django.utils.timezone.now, blank=True, editable=False)),
+                ('description', models.TextField(help_text='This experiment description', verbose_name='Description', null=True)),
+                ('algorithm', models.CharField(help_text='Name of algorithm', max_length=255, verbose_name='Algorithm', null=True)),
+                ('neighbours', models.CharField(help_text='Strategy how neighbours are determined', max_length=255, verbose_name='Neighbours', null=True)),
+                ('stopping_criteria', models.CharField(help_text='Stopping criteria strategy', max_length=255, verbose_name='Stopping criteria', null=True)),
+                ('stopping_accuracy', models.CharField(help_text='Stopping accuracy', max_length=255, verbose_name='Stopping accuracy', null=True)),
+                ('subregion', models.CharField(help_text='Subregion type: simplex or rectangle', max_length=255, verbose_name='Subregion', null=True)),
+                ('inner_problem_accuracy', models.CharField(help_text='Inner problem solution accuracy', max_length=255, verbose_name='Inner accuracy', null=True)),
+                ('inner_problem_iters', models.IntegerField(help_text='Inner problem maximum iterations to get solution', verbose_name='Inner iters', null=True)),
+                ('inner_problem_division', models.CharField(help_text='Inner problem subregion division strategy', max_length=255, verbose_name='Division strategy in inner problem', null=True)),
+                ('lipschitz_estimation', models.CharField(help_text='Subregion Lipschitz constant estimation strategy', max_length=255, verbose_name='Lipschitz estimation', null=True)),
+                ('simplex_division', models.CharField(help_text='Subregion division strategy', max_length=255, verbose_name='Simplex division', null=True)),
+                ('valid', models.BooleanField(default=True, help_text='Is this experiment valid? Its not valid if critical mistake was found.', verbose_name='Valid')),
+                ('mistakes', models.TextField(help_text='Descriptions of mistakes, which were found in this experiment', verbose_name='Mistakes', null=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Task',
+            fields=[
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
+                ('func_name', models.CharField(help_text='Function name', max_length=255, verbose_name='Function name', null=True)),
+                ('func_cls', models.IntegerField(help_text='GKLS function class', verbose_name='GKLS class', null=True)),
+                ('func_id', models.IntegerField(help_text='GKLS function id', verbose_name='GKLS id', null=True)),
+                ('calls', models.IntegerField(help_text='Calls', verbose_name='Calls', null=True)),
+                ('subregions', models.IntegerField(help_text='Subregions in final partition', verbose_name='Subregions', null=True)),
+                ('duration', models.DurationField(help_text='Duration of task execution', verbose_name='Duration', null=True)),
+                ('f_min', models.FloatField(help_text='Value of minimum, which managed to determine', verbose_name='F min', null=True)),
+                ('x_min', models.CharField(help_text='Determined minimum coordinates', max_length=255, verbose_name='X min', null=True)),
+                ('experiment', models.ForeignKey(related_name='tasks', help_text='ID of experiment to which this task is assigned to', null=True, to='core.Experiment')),
             ],
         ),
     ]
--- dakis/core/models.py
+++ dakis/core/models.py
@@ -10,12 +10,57 @@ from django.utils.translation import ugettext_lazy as _
 class Experiment(models.Model):
     created = CreationDateTimeField()
     modified = ModificationDateTimeField()
-    slug = AutoSlugField(populate_from='title')
-    title = models.CharField(_("Title"), max_length=255)
-    description = models.TextField(_("Description"))
+
+    description = models.TextField(_('Description'), null=True,
+        help_text=_('This experiment description'))
+
+    algorithm = models.CharField(_('Algorithm'), max_length=255, null=True,
+        help_text=_('Name of algorithm'))
+
+    neighbours = models.CharField(_('Neighbours'), max_length=255, null=True,
+        help_text=_('Strategy how neighbours are determined'))
+
+    stopping_criteria = models.CharField(_('Stopping criteria'), max_length=255, null=True,
+        help_text=_('Stopping criteria strategy'))
+    stopping_accuracy = models.CharField(_('Stopping accuracy'), max_length=255, null=True,
+        help_text=_('Stopping accuracy'))
+
+    subregion = models.CharField(_('Subregion'), max_length=255, null=True,
+        help_text=_('Subregion type: simplex or rectangle'))
+
+    inner_problem_accuracy = models.CharField(_('Inner accuracy'), max_length=255, null=True,
+        help_text=_('Inner problem solution accuracy'))
+    inner_problem_iters = models.IntegerField(_('Inner iters'), null=True,
+        help_text=_('Inner problem maximum iterations to get solution'))
+    inner_problem_division = models.CharField(_('Division strategy in inner problem'), max_length=255, null=True,
+        help_text=_('Inner problem subregion division strategy'))
+
+    lipschitz_estimation = models.CharField(_('Lipschitz estimation'), max_length=255, null=True,
+        help_text=_('Subregion Lipschitz constant estimation strategy'))
+
+    simplex_division = models.CharField(_('Simplex division'), max_length=255, null=True,
+        help_text=_('Subregion division strategy'))
+
+    valid = models.BooleanField(_('Valid'), default=True,
+        help_text=_('Is this experiment valid? Its not valid if critical mistake was found.'))
+    mistakes = models.TextField(_('Mistakes'), null=True,
+        help_text=_('Descriptions of mistakes, which were found in this experiment'))
 
     def __str__(self):
-        return self.title
+        return '%s  %s  %s  %s' % (self.algorithm, self.neighbours, self.subregion, self.stopping_accuracy)
 
     def get_absolute_url(self):
-        return reverse('project-details', args=[self.slug])
+        return reverse('experiment-details', args=[self.pk])
+
+
+class Task(models.Model):
+    func_name = models.CharField(_('Function name'), null=True, max_length=255, help_text=_('Function name'))
+    func_cls = models.IntegerField(_('GKLS class'), null=True, help_text=_('GKLS function class'))
+    func_id = models.IntegerField(_('GKLS id'), null=True, help_text=_('GKLS function id'))
+    calls = models.IntegerField(_('Calls'), null=True, help_text=_('Calls'))
+    subregions = models.IntegerField(_('Subregions'), null=True, help_text=_('Subregions in final partition'))
+    duration = models.DurationField(_('Duration'), null=True, help_text=_('Duration of task execution'))
+    f_min = models.FloatField(_('F min'), null=True, help_text=_('Value of minimum, which managed to determine'))
+    x_min = models.CharField(_('X min'), max_length=255, null=True, help_text=_('Determined minimum coordinates'))
+    experiment = models.ForeignKey('Experiment', related_name='tasks', null=True,
+                    help_text=_('ID of experiment to which this task is assigned to'))
