87018efa46dadf7213ce53d86540be54af9aee8b
niekas <albertas.gimbutas@mii.vu.lt>
2015-08-23 01:24:37 +0300

    Add missing reverse import

--- dakis/website/views.py
+++ dakis/website/views.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import redirect
