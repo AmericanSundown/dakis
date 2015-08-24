6c95034a708253b802cc57fa8079c0445a6fc657
niekas <albertas.gimbutas@mii.vu.lt>
2015-08-24 07:25:19 +0300

    Use float for duration, fix status type in worker

--- scripts/worker.py
+++ scripts/worker.py
@@ -7,18 +7,15 @@ import datetime
 import pytimeparse
 
 
-def timedelta_format(string):
-    return datetime.timedelta(seconds=pytimeparse.timeparse.timeparse(string))
-
 parser = argparse.ArgumentParser(description='Schedules tasks')
 parser.add_argument('-exp', '--exp_id', type=int, help='Experiment ID', nargs='?', default=None)
 parser.add_argument('-exe', '--executable', type=str, help='Executable file', nargs='?', default=None)
 
 parser.add_argument('-task', '--task_id', type=int, help='Task ID', nargs=None, default=None)
 parser.add_argument('-calls', '--calls', type=int, help='Number of calls', nargs=None, default=None)
-parser.add_argument('-duration', '--duration', type=timedelta_format, help='Execution duration', nargs=None, default=None)
+parser.add_argument('-duration', '--duration', type=float, help='Execution duration', nargs=None, default=None)
 parser.add_argument('-subs', '--subregions', type=int, help='Number of subregions', nargs=None, default=None)
-parser.add_argument('-st', '--status', type=int, help='Status of the task. D - done, S - suspended.', nargs=None, default=None)
+parser.add_argument('-st', '--status', type=str, help='Status of the task. D - done, S - suspended.', nargs=None, default=None)
 
 
 def run_next_task(exp_id, executable):
@@ -28,10 +25,10 @@ def run_next_task(exp_id, executable):
     if resp.status_code == 200 and resp.json():
         subprocess.Popen([
             './%s' % executable,
-            '-cls=%d' % task['func_cls'],
-            '-fid=%d' % task['func_id'],
-            '-id=%d' % task['id'],
-            '--callback=%s' % sys.argv[0],
+            '--gkls_cls=%d' % task['func_cls'],
+            '--gkls_fid=%d' % task['func_id'],
+            '--task_id=%d' % task['id'],
+            '--callback=%s' % sys.argv[0].strip('./'),
         ],
         stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
 
