eba2f5b2717ea9e9bc59a53e2a3b03e06f1e8587
niekas <albertas.gimbutas@mii.vu.lt>
2015-08-29 06:01:31 +0300

    Add Feedback menu item

--- dakis/website/menus.py
+++ dakis/website/menus.py
@@ -3,18 +3,22 @@ from django.utils.translation import ugettext_lazy as _
 
 
 class Item(object):
-    def __init__(self, label, name, *args, **kwargs):
+    def __init__(self, label, name=None, url=None, *args, **kwargs):
         self.label = label
         self.name = name
+        self.address = url
         self.args = args
         self.kwargs = kwargs
 
     def url(self):
+        if self.address:
+            return self.address
         return reverse(self.name, args=self.args, kwargs=self.kwargs)
 
 
 menus = {
     'topmenu': [
-        Item(_('New'), 'experiment-list'),
+        Item(_('New'), name='experiment-list'),
+        Item(_('Feedback'), url='https://github.com/niekas/dakis/issues/new'),
     ],
 }
