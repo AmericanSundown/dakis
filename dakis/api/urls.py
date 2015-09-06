7b1104aa0ecb0e57c49510eab6ebd7d9eb35382f
niekas <albertas.gimbutas@mii.vu.lt>
2015-09-07 00:15:00 +0300

    Add User model to REST API
    And fix logout redirect url.

--- dakis/api/urls.py
+++ dakis/api/urls.py
@@ -9,6 +9,7 @@ import dakis.api.views as api_views
 router = routers.DefaultRouter()
 router.register(r'experiments', api_views.ExperimentViewSet)
 router.register(r'tasks', api_views.TaskViewSet)
+router.register(r'users', api_views.UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- dakis/api/views.py
+++ dakis/api/views.py
@@ -1,5 +1,7 @@
 from rest_framework import serializers, viewsets
 
+from django.contrib.auth.models import User
+
 from dakis.core.models import Experiment, Task
 
 
@@ -11,6 +13,10 @@ class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+
 
 class ExperimentViewSet(viewsets.ModelViewSet):
     queryset = Experiment.objects.all()
@@ -19,3 +25,7 @@ class ExperimentViewSet(viewsets.ModelViewSet):
 class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
