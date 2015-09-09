9f7e831b4cdd197d977731e629960185afe81b2a
niekas <albertas.gimbutas@mii.vu.lt>
2015-09-09 15:39:51 +0300

    Source /etc/profile when using non-interactive ssh

--- dakis/website/views.py
+++ dakis/website/views.py
@@ -49,7 +49,7 @@ def toggle_exp_status(request, exp_id):
 
 def run_worker(exp, user):
     cmd = 'sshpass -p%s ssh -o "StrictHostKeyChecking no" %s ' \
-          '".dakis/worker.py -exp=%d -exe=%s -rep=%s -br=%s -j"' % (
+          '"source /etc/profile; .dakis/worker.py -exp=%d -exe=%s -rep=%s -br=%s -j"' % (
         user.profile.host_password,
         user.profile.hostname,
         exp.pk,
