686930cb4282e11449894f23d03ad801edd2b775
niekas <albertas.gimbutas@mii.vu.lt>
2015-11-26 18:15:11 +0200

    Add ability to select exps for comparison

--- dakis/website/static/js/colapse.js
+++ dakis/website/static/js/colapse.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+    var selected_exps = [];
+
+    // Colapse
     $('.show_children').click(function(){
         $(this).parent().next('.children').css('display', 'inline');
         $(this).parent().find('.show_children').css('display', 'none');
@@ -9,4 +12,17 @@ $(document).ready(function() {
         $(this).parent().find('.show_children').css('display', 'inline');
         $(this).parent().find('.hide_children').css('display', 'none');
     });
+
+    // Select experiments
+    $('.exp').click(function() {
+        var exp_pk = $(this).attr('exp_pk');
+        if ($(this).is(':checked')) {
+            selected_exps.push(exp_pk);
+        } else {
+            selected_exps.pop(exp_pk);
+        };
+        var href = $('#comparison-url').attr('href').split('=')[0];
+        $('#comparison-url').attr('href', href+'='+selected_exps);
+    });
+
 });
