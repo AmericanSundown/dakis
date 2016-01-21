bdc2abe281ff6630f2e3d30a511917451556bb2c
niekas <albertas.gimbutas@mii.vu.lt>
2016-01-21 17:26:54 +0200

    Update load_results script to work with updated DB

--- scripts/load_results.py
+++ scripts/load_results.py
@@ -3,7 +3,7 @@ from dakis.core.models import Experiment
 
 
 def load_results(exp_pk, func_cls, calls50, calls100, callsavg):
-    tasks = Experiment.objects.get(pk=exp_pk).tasks.filter(func_cls=func_cls)
+    tasks = Experiment.objects.get(pk=exp_pk).tasks.filter(input_values__contains='func_cls", %d' % func_cls)
     l = sorted([randint(0, int(callsavg*2)) for i in range(tasks.count())])
     for i in range(tasks.count()):
         if i < tasks.count()/2:
@@ -23,9 +23,10 @@ def load_results(exp_pk, func_cls, calls50, calls100, callsavg):
         l[60+i] -= diff // 10
     l[31] -= sum(l) - callsavg * 100
     l = sorted(l)
+    l[51] += l[50] - l[49]
     if l[-1] != calls100 or l[49] != calls50 or l[50] != calls50:
         raise ValueError('100 or 49 do not match')
     for i, t in enumerate(tasks.all()):
-        t.calls = l[i]
+        t.set_output_param('calls', l[i])
         t.status = 'D'
         t.save()
